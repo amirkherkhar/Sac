b321aa9ad197368c7ac344d18dfa54ef560bd949
amir <kherkhar.amir@wissalacademy.com>
2024-11-06 11:08:08 +0100

    push

---
+++ src/test/java/com/example/jiri/JiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
